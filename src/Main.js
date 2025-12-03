2be5ca052627d00ef69cf9f27dc0733768566c4d
starkmar3 <mari.gudyma@yandex.ru>
2025-12-03 21:04:28 +0500

    Add files via upload

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.css';
+import Product from './Product';
+
+function Main() {
+  return (
+    <div className="Main">
+        <Product />
+        <Product />
+        <Product />
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Product.css';
+
+function Product() {
+  return (
+    <div className="Product">
+        <p>Наш кофейный дом — это уютное убежище, где каждая страница любимой книги раскрывается заново благодаря чарующему аромату свежезаваренного кофе и атмосфере абсолютного покоя.</p>
+    </div>
+  );
+}
+
+export default Product;
