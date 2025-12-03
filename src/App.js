f1ef845d670c893e7227e8fdf11a9b3da6ec7776
starkmar3 <mari.gudyma@yandex.ru>
2025-12-03 20:53:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
+import Product from './Product';
+import Reviews from './Reviews';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Product />
+      <Reviews/>
+      <Footer />
     </div>
-  );
+  )
+
 }
 
 export default App;
